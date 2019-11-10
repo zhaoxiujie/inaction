8586dc96b419fee95e14f57766f639ca0e339345
zhaoxiujie1 <zhaoxiujie@jd.com>
2019-11-10 16:18:41 +0800

    init project

---
+++ inaction-dao/src/main/java/com/zxj/inaction/dao/InactionDaoApplication.java
@@ -0,0 +1,13 @@
+package com.zxj.inaction.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InactionDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InactionDaoApplication.class, args);
+    }
+
+}
---
+++ inaction-web/src/test/java/com/zxj/inaction/web/InactionWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxj.inaction.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InactionWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
