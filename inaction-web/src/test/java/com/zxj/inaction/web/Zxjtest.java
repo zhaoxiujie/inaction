ec58d7f82c93e2832d09ed94697552d735025956
zhaoxiujie1 <zhaoxiujie@jd.com>
2020-08-03 10:19:18 +0800

    add alibaba fastjson dependency

---
+++ inaction-web/src/test/java/com/zxj/inaction/web/Zxjtest.java
@@ -0,0 +1,28 @@
+package com.zxj.inaction.web;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class Zxjtest {
+    /**
+     * 当天+next天的0点0分
+     */
+    public static Date getDayStart(Date date, int next) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.add(Calendar.DAY_OF_MONTH, next);
+        return calendar.getTime();
+    }
+
+    public static void main(String[] args) {
+        Date today = getDayStart(new Date(), -1);
+        System.out.println(JSON.toJSONString(today));
+    }
+}
